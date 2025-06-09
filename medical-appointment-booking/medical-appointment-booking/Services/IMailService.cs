8a010a1fec89e92947a019f6145da4db4a95cdc6
admin <admin@LAPTOP-56OHGFRG>
2025-06-09 21:16:13 +0700

    reset

--- medical-appointment-booking/medical-appointment-booking/Services/IMailService.cs
+++ medical-appointment-booking/medical-appointment-booking/Services/IMailService.cs
@@ -1,6 +1,6 @@
 ﻿namespace medical_appointment_booking.Services
 {
-    public interface IMailService1
+    public interface IMailService
     {
         Task SendEmailWelcome(string to, string name, string phone, DateTime registrationDate);
         Task SendDoctorWelcomeEmail(string to, string doctorName, string email, string temporaryPassword,
