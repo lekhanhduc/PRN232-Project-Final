e68ff097b75bf7f51c96c8831784bce5be2904f0
duclk <lekhanhduccc@gmail.com>
2025-06-18 19:20:32 +0700

    Update MFT and Create Doctor

--- medical-appointment-booking/medical-appointment-booking/Services/IMailService.cs
+++ medical-appointment-booking/medical-appointment-booking/Services/IMailService.cs
@@ -3,5 +3,7 @@
     public interface IMailService
     {
         Task SendEmailWelcome(string to, string name, string phone, DateTime registrationDate);
+        Task SendDoctorWelcomeEmail(string to, string doctorName, string email, string temporaryPassword,
+            string licenseNumber, string specialty, decimal consultationFee, DateTime registrationDate);
     }
 }
