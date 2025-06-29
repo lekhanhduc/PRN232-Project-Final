157b5e13050f4c47917bc0a4aa1e8407aabcd1f9
admin <admin@LAPTOP-56OHGFRG>
2025-06-29 16:25:29 +0700

    commit 29/06

--- medical-appointment-booking/medical-appointment-booking/Repositories/DoctorRepository.cs
+++ medical-appointment-booking/medical-appointment-booking/Repositories/DoctorRepository.cs
@@ -28,6 +28,40 @@ namespace medical_appointment_booking.Repositories
             await context.SaveChangesAsync();
         }
 
+        public async Task<List<Doctor>> GetAll(int page, int size, string? keyword = null)
+        {
+            if (page < 1)
+            {
+                page = 1;
+            }
+            if (size > 100) size = 100;
+
+            var query = context.Doctors.Include(d => d.Specialty).Include(s => s.User).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(s => s.FirstName.Contains(keyword) || s.LastName.Contains(keyword));
+            }
+
+            return await query
+                .OrderBy(s => s.FirstName)
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync();
+        }
+
+        public async Task<long> GetTotalCount(string? keyword = null)
+        {
+            var query = context.Doctors.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(s => s.FirstName.Contains(keyword) || s.LastName.Contains(keyword));
+            }
+
+            return await query.LongCountAsync();
+        }
+
         public async Task<Doctor?> GetDoctorByIdAsync(long id)
         {
             return await context.Doctors
@@ -38,7 +72,7 @@ namespace medical_appointment_booking.Repositories
 
         public async Task<long> ToTalDoctorsBySpecialty(int specialty)
         {
-            return await context.Doctors .Include(d => d.Specialty)
+            return await context.Doctors.Include(d => d.Specialty)
                 .Include(u => u.User)
                 .Where(d => d.SpecialtyId == specialty)
                 .CountAsync();
@@ -47,8 +81,8 @@ namespace medical_appointment_booking.Repositories
         public async Task<Doctor?> GetDoctorByLicenseNumber(string licenseNumber)
         {
             return await context.Doctors.Include(d => d.User)
-                .Include (d => d.Specialty)
-                .FirstOrDefaultAsync (d => d.LicenseNumber == licenseNumber);
+                .Include(d => d.Specialty)
+                .FirstOrDefaultAsync(d => d.LicenseNumber == licenseNumber);
         }
 
         public async Task<bool> DeleteDoctorAsync(long id)
@@ -60,6 +94,12 @@ namespace medical_appointment_booking.Repositories
             context.Doctors.Remove(doctor);
             await context.SaveChangesAsync();
             return true;
+        }
+
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            context.Doctors.Update(doctor);
+            await context.SaveChangesAsync();
         }
 
     }
