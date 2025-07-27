b2d07a522168058de8a179964f4f242b6e06dddb
duclk <lekhanhduccc@gmail.com>
2025-07-27 18:30:18 +0700

    KhanhDuc Fix Conflict

---
+++ medical-appointment-booking/medical-appointment-booking/Repositories/ServicePackageRepository.cs
@@ -0,0 +1,23 @@
+﻿using medical_appointment_booking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace medical_appointment_booking.Repositories
+{
+    public class ServicePackageRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ServicePackageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ServicePackage>> GetActiveAsync()
+        {
+            return await _context.ServicePackages
+                .Where(sp => sp.IsActive)
+                .OrderBy(sp => sp.PackageName)
+                .ToListAsync();
+        }
+    }
+}
---
+++ medical-appointment-booking/medical-appointment-booking/Services/IServicePackageService.cs
@@ -0,0 +1,9 @@
+﻿using medical_appointment_booking.Dtos.Response;
+
+namespace medical_appointment_booking.Services
+{
+    public interface IServicePackageService
+    {
+        Task<IEnumerable<ServicePackageResponse>> GetServicePackagesAsync();
+    }
+}
