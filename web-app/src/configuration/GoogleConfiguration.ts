c7858b2280782e2843090743d2289f6559c303d4
duclk <lekhanhduccc@gmail.com>
2025-07-28 10:58:31 +0700

    Update env

--- web-app/src/configuration/GoogleConfiguration.ts
+++ web-app/src/configuration/GoogleConfiguration.ts
@@ -2,5 +2,5 @@ export const GoogleConfiguration = {
     client_id: process.env.GOOGLE_CLIENT_ID || "441587123979-qds882ebt12bna4t4pldj9ausd2udpu2.apps.googleusercontent.com",
     response_type: process.env.GOOGLE_RESPONSE_TYPE || "code",
     scope: process.env.GOOGLE_SCOPE || "email%20profile%20openid",
-    redirect_uri: process.env.GOOGLE_REDIRECT_URI || "https://api.taycodejava.id.vn/oauth2/callback/google",
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI || "https://taycodejava.id.vn/oauth2/callback/google",
 };
